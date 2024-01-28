baa83288027041bb28d7b44868e145096181db9f
stappost <emanuele.scarpantoni@gmail.com>
2024-01-28 13:24:19 +0100

    add form

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Genera la tua password</h1>
+    <form action="./index.php" methods="GET">
+        <input type="number" placeholder='Lunghezza password da generare' name='length' id='length'>
+    </form>
+</body>
+</html>
