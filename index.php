8e2ab12b9095f332a1fc7141b37a88f04f233c74
stappost <emanuele.scarpantoni@gmail.com>
2024-01-28 13:39:48 +0100

    end first milestone

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
-
+    if(isset($_GET['length'])){
+        // CARATTERI
+        $allChar = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?!.,/[]{}&@#';
+        // LUNGHEZZA DESIDERATA 
+        $passLen = $_GET['length'];
+        // PASSWORD DA RESTITURE 
+        $password = [];
+        // CICLO 
+        while(count($password) < $passLen){
+            $password[] = $allChar[rand(0, strlen($allChar))];
+        }
+        $password = implode($password);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -14,5 +26,7 @@
     <form action="./index.php" methods="GET">
         <input type="number" placeholder='Lunghezza password da generare' name='length' id='length'>
     </form>
+    <h2>Ecco la tua password</h2>
+    <h3><?php echo $password ?? 'no'?></h3>
 </body>
 </html>
